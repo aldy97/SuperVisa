9d3ecbb201309afc8b82f6ee1ad0e59c5f2180e5
feng <feng@pineappleexpressdelivery.com>
2021-06-11 10:51:08 -0400

    successfully get key

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import * as React from "react";
 import axios from "axios";
-import { BASE_URL } from "./utils/constants";
 
 const register = async () => {
   const request = {
-    username: "test",
-    email: "test@gmail.com",
-    password1: "1234567",
-    password2: "1234567",
+    username: "div1",
+    email: "div1@gmail.com",
+    password1: "1234567!",
+    password2: "1234567!",
   };
 
-  const URL = `${BASE_URL}/auth/registration/`;
-  const response = await axios.post(URL, request);
-  console.log(response);
+  try {
+    const URL = "https://sv-survey.herokuapp.com/auth/registration/";
+    const response = await axios.post(URL, request);
+    console.log(response.data.key);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 function App() {
   React.useEffect(() => {
     register();
   }, []);
+
   return <div className="App"></div>;
 }
 
