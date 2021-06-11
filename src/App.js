683d0f110665a1305f99018d6b3ecd300f191866
Feng Xiong <fengxiong34@gmail.com>
2021-06-10 22:27:51 -0400

    added axios for sending request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import * as React from "react";
+import axios from "axios";
+import { BASE_URL } from "./utils/constants";
+
+const register = async () => {
+  const request = {
+    username: "test",
+    email: "test@gmail.com",
+    password1: "1234567",
+    password2: "1234567",
+  };
+
+  const URL = `${BASE_URL}/auth/registration/`;
+  const response = await axios.post(URL, request);
+  console.log(response);
+};
+
 function App() {
+  React.useEffect(() => {
+    register();
+  }, []);
   return <div className="App"></div>;
 }
 
